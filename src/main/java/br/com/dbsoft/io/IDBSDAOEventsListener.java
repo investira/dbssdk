b076bf130d981c84266f98cb2e2515c8258086dc
rvillar <ricardo.villar@dbsoft.com.br>
2014-10-14 15:54:47 -0300

    Documentação

--- src/main/java/br/com/dbsoft/io/IDBSDAOEventsListener.java
+++ src/main/java/br/com/dbsoft/io/IDBSDAOEventsListener.java
@@ -3,13 +3,13 @@ package br.com.dbsoft.io;
 public interface IDBSDAOEventsListener {
 	
 	/**
-	 * Chamado antes de exeutar o open(Recorset ou file)
+	 * Chamado antes de executar o open(Recorset ou file)
 	 * No DAO com <b>setType(TYPE.FIXED_COLUMNS)<b/> este evento deve ser utilizado para configurar as colunas fixas;
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void beforeOpen(DBSDAOEvent pEvent);
 	/**
-	 * Chamado depois de exeutar a pesquisa SQL do Open ou Refresh.<br/>
+	 * Chamado depois de executar a pesquisa SQL do Open ou Refresh.<br/>
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void afterOpen(DBSDAOEvent pEvent);
@@ -26,57 +26,57 @@ public interface IDBSDAOEventsListener {
 	public abstract void afterClose(DBSDAOEvent pEvent);
 
 	/**
-	 * Chamado antes de exeutar o comando de insert
+	 * Chamado antes de executar o comando de insert
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void beforeInsert(DBSDAOEvent pEvent);
 	/**
-	 * Chamado depois de exeutar a pesquisa SQL do Open ou Refresh
+	 * Chamado depois de executar a pesquisa SQL do Open ou Refresh
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void afterInsert(DBSDAOEvent pEvent);
 	
 	/**
-	 * Chamado antes de exeutar de leitura de um registro
+	 * Chamado antes de executar de leitura de um registro
 	 * Neste momento, pode-se reconfigurar o layout das colunas fixas no DBSDBSTxt.
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void beforeRead(DBSDAOEvent pEvent);
 	/**
-	 * Chamado depois de exeutar a leitura de um registro
+	 * Chamado depois de executar a leitura de um registro
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void afterRead(DBSDAOEvent pEvent);
 	
 	/**
-	 * Chamado antes de exeutar o comando de update
+	 * Chamado antes de executar o comando de update
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void beforeUpdate(DBSDAOEvent pEvent);
 	/**
-	 * Chamado depois de exeutar o update
+	 * Chamado depois de executar o update
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void afterUpdate(DBSDAOEvent pEvent);
 	
 	/**
-	 * Chamado antes de exeutar o comando de merge. Não disparando os eventos de insert e update
+	 * Chamado antes de executar o comando de merge. Não disparando os eventos de insert e update
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void beforeMerge(DBSDAOEvent pEvent);
 	/**
-	 * Chamado depois de exeutar o merge(Inclui se não existe/Altera se existe)
+	 * Chamado depois de executar o merge(Inclui se não existe/Altera se existe)
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void afterMerge(DBSDAOEvent pEvent);
 	
 	/**
-	 * Chamado antes de exeutar o comando de delete
+	 * Chamado antes de executar o comando de delete
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void beforeDelete(DBSDAOEvent pEvent);
 	/**
-	 * Chamado depois de exeutar o comando de delete
+	 * Chamado depois de executar o comando de delete
 	 * @param pEvent Informações do evento
 	 */
 	public abstract void afterDelete(DBSDAOEvent pEvent);
