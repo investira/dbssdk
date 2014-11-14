022ef4c7b32ca99edbc6ae0d00c181df576b1f4b
rvillar <ricardo.villar@dbsoft.com.br>
2014-11-14 16:52:44 -0200

    testes

--- src/test/java/br/com/dbsoft/tmp/lixoIO.java
+++ src/test/java/br/com/dbsoft/tmp/lixoIO.java
@@ -1,63 +1,71 @@
 package br.com.dbsoft.tmp;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
+import java.sql.DriverManager;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import br.com.dbsoft.core.DBSSDK;
+import br.com.dbsoft.error.DBSIOException;
+import br.com.dbsoft.io.DBSDAO;
+import br.com.dbsoft.util.DBSIO;
 
 
 
-//@RunWith(Arquillian.class)
 public class lixoIO  {
 
-	//É necessário adicionar as classes que serão utilizadas com "addClass" conforme exemplo abaixo 
-//	@Deployment
-//    public static WebArchive createDeployment() {
-//		
-////		MavenDependencyResolver xResolver = DependencyResolvers.use(MavenDependencyResolver.class).loadMetadataFromPom("pom.xml").goOffline();
-//		
-//		WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
-//			.addPackage("br.com.dbsoft.event")
-//			.addPackage("br.com.dbsoft.annotation")
-//			.addPackage("br.com.dbsoft.core")
-//			.addPackage("br.com.dbsoft.error")
-//			.addPackage("br.com.dbsoft.factory")
-//			.addPackage("br.com.dbsoft.io")
-//			.addPackage("br.com.dbsoft.util")
-//			.addPackage("br.com.dbsoft.dao")
-////			.addAsLibraries(xResolver.artifact("joda-time:joda-time:2.1").resolveAsFiles()) // groupId:artifactId:version conforme definido no pom
-//            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-//		return war;
-//    }
+//	String url = "jdbc:mysql://ifeed.com.br:3306/ifeed?zeroDateTimeBehavior=convertToNull&amp;useOldAliasMetadataBehavior=true";
+//	String url = "jdbc:mysql://ifeed.com.br:3306/dbsfnd?zeroDateTimeBehavior=convertToNull&amp;useOldAliasMetadataBehavior=true";
+//	String url="jdbc:mysql://localhost:3306/dbsfnd?zeroDateTimeBehavior=convertToNull&amp;useOldAliasMetadataBehavior=true";
+//	String url="jdbc:mysql://localhost:3306/ifeed?zeroDateTimeBehavior=convertToNull&amp;useOldAliasMetadataBehavior=true";
+//	String url="jdbc:mysql://192.168.0.106:3306/dbsfnd?zeroDateTimeBehavior=convertToNull&amp;useOldAliasMetadataBehavior=true";
+//	String url="jdbc:mysql://192.168.0.106:3306/ifeed?zeroDateTimeBehavior=convertToNull&amp;useOldAliasMetadataBehavior=true";
+//	String url="jdbc:oracle:thin:@192.168.0.20:1521:xe";
+	String url="jdbc:mysql://localhost:3306/junit?zeroDateTimeBehavior=convertToNull&amp;useOldAliasMetadataBehavior=true";
+	String user="teste";
+	String password="teste";
+	Connection wConexao;
+	Connection wConexao2;
 
-
-//	@Resource(mappedName="junit")
-	private DataSource wDS;
-
-	Connection wCn;
-	
 	@Before
-	public void openCn(){
-		try {
-			wCn = wDS.getConnection();
-			wCn.setAutoCommit(false);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public void setup() throws Exception {
+		Class.forName(DBSSDK.JDBC_DRIVER.MYSQL);
+		Class.forName(DBSSDK.JDBC_DRIVER.ORACLE);
+		wConexao = DriverManager.getConnection(url, user, password);
+		wConexao.setAutoCommit(false);
+		wConexao2 = DriverManager.getConnection(url, user, password);
+		wConexao2.setAutoCommit(false);
 	}
 	
 	@After
-	public void closeCn(){
-		try {
-			wCn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+	public void tearDown() throws Exception {
+		wConexao.close();
+	}	
 	
+	@Test
+	public void DeadLock() throws DBSIOException{
+		DBSIO.beginTrans(wConexao);
+		DBSIO.beginTrans(wConexao2);
+		String xSQL = "Select * From Crud where crud_id = 1 ";
+		DBSDAO<Object> xDAO = new DBSDAO<Object>(wConexao, "CRUD");
+		DBSDAO<Object> xDAO2 = new DBSDAO<Object>(wConexao2, "CRUD");
+		if (xDAO.open(xSQL)){
+			xDAO.setValue("CRUD", "CRUD 23");
+			xDAO.executeUpdate();
+			xDAO.close();
+			if (xDAO2.open(xSQL)){
+				xDAO2.setValue("CRUD", "CRUD 24");
+				xDAO2.executeUpdate();
+				xDAO2.close();
+			}
+			xDAO.close();
+		}
+		DBSIO.endTrans(wConexao, true);
+		DBSIO.endTrans(wConexao2, true);
+		
+	}
 	
 
 	
