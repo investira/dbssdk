1ac15cfc11a503afe9e7cf4747efc6094a2dc259
rvillar <ricardo.villar@dbsoft.com.br>
2015-03-27 14:00:00 -0300

    encode com opção de inclusão de atributos

--- src/main/java/br/com/dbsoft/util/DBSHtml.java
+++ src/main/java/br/com/dbsoft/util/DBSHtml.java
@@ -8,10 +8,21 @@ public class DBSHtml {
 	 * @param pTag
 	 * @param pString
 	 */
-	public static String encodeTag(String pTag, String pValue){
-		if (pTag == null){return null;}
-		return encodeTagBegin(pTag) + pValue + encodeTagEnd(pTag);
+	public static String encodeTag(String pTag, Object pValue){
+		return encodeTag(pTag, pValue, null);
 	}
+
+	/**
+	 * Retorna html contendo o valor em informado <b>pValue<b/> dento do tab informada em <b>pTab<b/>.<br/>
+	 * O tab <b>não</b> deve vir com os sinais <>.
+	 * @param pTag
+	 * @param pString
+	 */
+	public static String encodeTag(String pTag, Object pValue, String pAttributes){
+		if (pTag == null){return null;}
+		return encodeTagBegin(pTag, pAttributes) + DBSString.toString(pValue, "") + encodeTagEnd(pTag);
+	}
+
 	/**
 	 * Retorna html contendo o valor em informado <b>pValue<b/> dento do tab informada em <b>pTab<b/>.<br/>
 	 * O tab <b>não</b> deve vir com os sinais <>.
@@ -19,9 +30,20 @@ public class DBSHtml {
 	 * @param pString
 	 */
 	public static String encodeTagBegin(String pTag){
-		if (pTag == null){return null;}
-		return "<" + pTag + ">";
+		return encodeTagBegin(pTag, null);
 	}
+
+	/**
+	 * Retorna html contendo o valor em informado <b>pValue<b/> dento do tab informada em <b>pTab<b/>.<br/>
+	 * O tab <b>não</b> deve vir com os sinais <>.
+	 * @param pTag
+	 * @param pString
+	 */
+	public static String encodeTagBegin(String pTag, String pAttributes){
+		if (pTag == null){return null;}
+		return "<" + pTag + " " + DBSString.getNotNull(pAttributes, "") + ">";
+	}
+
 	/**
 	 * Retorna html contendo o valor em informado <b>pValue<b/> dento do tab informada em <b>pTab<b/>.<br/>
 	 * O tab <b>não</b> deve vir com os sinais <>.
@@ -32,4 +54,10 @@ public class DBSHtml {
 		if (pTag == null){return null;}
 		return "</" + pTag + ">";
 	}
+
+	public static String encodeTagBR(){
+		return "<br/>";
+	}
+	
+	
 }
