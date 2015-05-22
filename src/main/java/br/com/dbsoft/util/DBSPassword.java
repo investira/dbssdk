57b551b412f0c77b372050fcd12023248c0d7f06
ricardo.villar <ricardo.villar@192.168.0.200>
2015-05-22 17:47:28 -0300

    metodos para verificar se senha contém caracteres válidos

--- src/main/java/br/com/dbsoft/util/DBSPassword.java
+++ src/main/java/br/com/dbsoft/util/DBSPassword.java
@@ -15,6 +15,10 @@ import org.apache.log4j.Logger;
  * @author ricardo.villar
  *
  */
+/**
+ * @author ricardo.villar
+ *
+ */
 public class DBSPassword {
 
 	protected static Logger	wLogger = Logger.getLogger(DBSPassword.class);
@@ -24,6 +28,7 @@ public class DBSPassword {
 	public static int		SaltLenghtInBytes = 15;
 	public static int		PasswordLenght = SaltLenghtInBytes * 2;
 	public static int		Iterations = 9000;
+	public static String	ValidsCharacters = "\\-!@#$%&_+=;:,.";
 	
     /**
      * Retorna string com valores em hexa, gerados randomicamente.
@@ -42,20 +47,31 @@ public class DBSPassword {
     }
 
     /**
-     * Retorna String com a senha criptografada a partir do texto informado adicionando um hash criado randomicamente(<b>salt</b>) para obrigar que todos os textos sejam sempre diferentes.<br/>
+     * Retorna String com a senha (de 30 caracteres) criptografada a partir do texto informado adicionando um hash criado randomicamente(<b>salt</b>) para obrigar que todos os textos sejam sempre diferentes.<br/>
      * Para a validação da senha porteriormente, deve-se utilizar o método<b>validateSaltedPassword</b>.
      * @param pPlainPassword
      * @param pHashLenght
      * @return
      */
     public static String createSaltedPassword(String pPlainPassword) {
+		return createSaltedPassword(pPlainPassword, PasswordLenght);
+	}
+
+    /**
+     * Retorna String com a senha criptografada a partir do texto informado adicionando um hash criado randomicamente(<b>salt</b>) para obrigar que todos os textos sejam sempre diferentes.<br/>
+     * Para a validação da senha porteriormente, deve-se utilizar o método<b>validateSaltedPassword</b>.
+     * @param pPlainPassword
+     * @param pHashLenght
+     * @return
+     */
+    public static String createSaltedPassword(String pPlainPassword, Integer pLength) {
 		String xSalt = createRandomSalt(); 
-    	byte[] xHash = pvGetHash(xSalt, pPlainPassword);
+    	byte[] xHash = pvGetHash(xSalt, pPlainPassword, pLength);
 		return DBSString.toHex(xHash) + xSalt;
 	}
 
     /**
-     * Retorna String com a senha criptografada a partir do texto informado adicionando utilizando um <b>salt</b>) padrão.<br/>
+     * Retorna String com a senha (de 30 caracteres) criptografada a partir do texto informado adicionando utilizando um <b>salt</b>) padrão.<br/>
      * Para a validação da senha porteriormente, deve-se utilizar o método<b>validatePassword</b>.
      * @param pPlainPassword
      * @return
@@ -70,6 +86,12 @@ public class DBSPassword {
 		return DBSString.toHex(xHash);
 	}
     
+    /**
+     * Verifica se a senha (de 30 caractes) informada em <b>pPlainPassword</b> é iqual a senha já criptografada informada em <b>pStoredPassword</b>.
+     * @param pPlainPassword
+     * @param pStoredPassword
+     * @return
+     */
     public static boolean validateSaltedPassword(String pPlainPassword, String pStoredPassword){
     	//Recupera parte da senha armazenada que é a efetiva senha do usuário
     	String xPassword = DBSString.getSubString(pStoredPassword, 1, PasswordLenght);
@@ -83,6 +105,12 @@ public class DBSPassword {
         return pvIsEqual(xPasswordHash, xStoredPassword);
     }
     
+    /**
+     * Verifica se a senha informada em <b>pPlainPassword</b> é iqual a senha já criptografada informada em <b>pStoredPassword</b>.
+     * @param pPlainPassword
+     * @param pStoredPassword
+     * @return
+     */
     public static boolean validatePassword(String pPlainPassword, String pStoredPassword){
       	//Converte para decimal
     	byte[] xStoredPassword = DBSString.fromHex(pStoredPassword);
@@ -91,6 +119,120 @@ public class DBSPassword {
          
         return pvIsEqual(xPasswordHash, xStoredPassword);
     }
+
+	/**
+	 * Retorna se senha contém os caracteres obrigatórios e válidos.<br/>
+	 * O tamanho mínimo é de 8 caracteres e máximo de 30 caracteres, 
+	 * sendo obrigatórios: uma letras maiúscula, uma letra minúscula e um números.<br/>
+	 * São permitidos a utilização dos sinais <b>-!@#$%&_+=.;:,<b/>.<br/>
+	 * @param pPassword Senha
+	 * @return
+	 */
+	public static Boolean checkPasswordContent(String pPassword){
+		return checkPasswordContent(pPassword, 8, 30);
+	}
+
+	/**
+	 * Retorna se senha contém os caracteres obrigatórios e válidos.<br/>
+	 * Caracteres obrigatórios: uma letras maiúscula, uma letra minúscula e um números.<br/>
+	 * São permitidos a utilização dos sinais <b>-!@#$%&_+=.;:,<b/>.
+	 * @param pPassword Senha
+	 * @param pMinSize Tamanho mínimo
+	 * @param pMaxSize Tamanho máximo
+	 * @return
+	 */
+	public static Boolean checkPasswordContent(String pPassword, Integer pMinSize, Integer pMaxSize){
+		return checkPasswordContent(pPassword, pMinSize, pMaxSize, true, true, true);
+	}
+
+	/**
+	 * Retorna se senha contém os caracteres obrigatórios e válidos.<br/>
+	 * Permitidos: letras, números e os sinais <b>-!@#$%&_+=.;:,<b/>.
+	 * @param pPassword Senha
+	 * @param pMinSize Tamanho mínimo
+	 * @param pMaxSize Tamanho máximo
+	 * @param pUpperCase Se deverá conter um caracter maiúsculo
+	 * @param pLowerCase Se deverá conter um caracter minúsculo
+	 * @param pNumber se deverá conter um número
+	 * @param pOthers
+	 * @return
+	 */
+	public static Boolean checkPasswordContent(String pPassword, Integer pMinSize, Integer pMaxSize, boolean pUpperCase, boolean pLowerCase, boolean pNumber){
+		return checkPasswordContent(pPassword, pMinSize, pMaxSize, pUpperCase, pLowerCase, pNumber, ValidsCharacters);
+	}
+
+	/**
+	 * Retorna se senha contém os caracteres obrigatórios e válidos.<br/>
+	 * Permitidos: letras, números e os sinais <b>-!@#$%&_+=.;:,<b/>.
+	 * @param pPassword Senha
+	 * @param pMinSize Tamanho mínimo
+	 * @param pMaxSize Tamanho máximo
+	 * @param pUpperCase Se deverá conter um caracter maiúsculo
+	 * @param pLowerCase Se deverá conter um caracter minúsculo
+	 * @param pNumber se deverá conter um número
+	 * @param pOthers
+	 * @return
+	 */
+	public static Boolean checkPasswordContent(String pPassword, Integer pMinSize, Integer pMaxSize, boolean pUpperCase, boolean pLowerCase, boolean pNumber, String pOthers){
+		if (pPassword == null
+		 || pMinSize == null 
+		 || pMaxSize == null
+		 || pMinSize > pMaxSize){
+			return false;
+		}
+		if (pPassword.length() < pMinSize
+		 || pPassword.length() > pMaxSize){
+			return false;
+		}
+		StringBuilder xRegex = new StringBuilder();
+		
+		/*
+		 * ^(
+		 * (?=.*\d)
+		 * (?=.*[a-z])
+		 * (?=.*[A-Z])
+		 * (?!.*\s)
+		 * .{8,20}
+		 * )[a-zA-Z0-9!@#$%~&*_+-=;:,.]$ 
+		 */
+//		 Tem que ter 1 A a
+		 //^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$
+		
+		//Precisa ter somente estes, senão vira vazio
+		//(^[a-zA-Z0-9]+$)
+		//(^[a-zA-Z0-9\-!@#$%&_+=;:,.]+$)
+		
+		//(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(^[a-zA-Z0-9\-!@#$%&_+=;:,.]+$)
+		
+//		  ^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,20})[a-zA-Z0-9\-!@#$%&_+=;:,.]$ 
+		
+		//(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(^[a-zA-Z0-9\-!@#$%&_+=;:,.]+$)
+		
+//		^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(^[a-zA-Z0-9\-!@#$%&_+=;:,.]+$)
+		
+		xRegex.append("^");
+		//Sem expaços
+		xRegex.append("(?!.*\\s)");
+		//Obriga uma letra maiúscula
+		if (pUpperCase){
+			xRegex.append("(?=.*[A-Z])");
+		}
+		//Obriga uma letra minúscula
+		if (pLowerCase){
+			xRegex.append("(?=.*[a-z])");
+		}
+		//Obriga um número
+		if (pNumber){
+			xRegex.append("(?=.*\\d)");
+		}
+		//Caracteres válidos
+		xRegex.append("(^[a-zA-Z0-9");
+		xRegex.append(DBSObject.getNotNull(pOthers, ""));
+		xRegex.append("]+$)");
+		
+		return pPassword.matches(xRegex.toString());
+	}
+	
 
     private static boolean pvIsEqual(byte[] xPasswordHash, byte[] pStoredPassword){
         int xDiff = pStoredPassword.length ^ xPasswordHash.length;
@@ -103,7 +245,7 @@ public class DBSPassword {
     
 
     /**
-     * Cria string hash criptografado a partir da string informada.
+     * Cria String (de 30 caracteres) hash criptografado a partir da string informada.
      * @param pSalt String adicional que será utilizada para criptografar a senha.
      * @param pPlainPassword Senha não criptografada
      * @return
@@ -129,36 +271,6 @@ public class DBSPassword {
 			return null;
 		}
 	}
-
-
-//    private static byte[] pvGetPlainText(String pSalt, String pHash, int pHashLenght) throws NoSuchAlgorithmException, InvalidKeySpecException{
-//    	SecretKeyFactory xFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-//    	xFactory.
-//    }
-
-    
-//    /**
-//     * Converte array de números decimais para valores em hexa.
-//     * @param pArray
-//     * @return
-//     * @throws NoSuchAlgorithmException
-//     */
-//    private static String pvToHex(byte[] pArray) throws NoSuchAlgorithmException{
-//        BigInteger xBigInteger = new BigInteger(1, pArray);
-//        String xHex = xBigInteger.toString(16);
-//        int xPaddingLength = (pArray.length * 2) - xHex.length();
-//        if(xPaddingLength > 0)
-//        {
-//            return String.format("%0"  +xPaddingLength + "d", 0) + xHex;
-//        }else{
-//            return xHex;
-//        }
-//    }
-    
-    
-    
-
-
 
 	
 }
