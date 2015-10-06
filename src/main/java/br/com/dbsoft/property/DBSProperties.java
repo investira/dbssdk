f606113e4281a8c2ade3a6e49e3818b0cd90cf41
ricardo.villar <ricardo.villar@192.168.0.200>
2015-10-06 14:08:50 -0300

    Alterado package da classe

---
+++ src/main/java/br/com/dbsoft/property/DBSProperties.java
@@ -0,0 +1,21 @@
+package br.com.dbsoft.property;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.TreeSet;
+
+/**
+ * Properties ordenadas alfabeticamente
+ * @author ricardo.villar
+ *
+ */
+public class DBSProperties extends Properties {
+
+	private static final long serialVersionUID = -7657143795257546946L;
+
+	@Override
+    public synchronized Enumeration<Object> keys() {
+        return Collections.enumeration(new TreeSet<Object>(super.keySet()));
+    }
+}
