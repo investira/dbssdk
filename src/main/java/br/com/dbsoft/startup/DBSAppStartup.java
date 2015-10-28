9df6e83068a57f01fc5a99419821bf923204530c
ricardo.villar <ricardo.villar@192.168.0.200>
2015-10-28 13:22:28 -0200

    Controle de startup da aplicação

--- src/main/java/br/com/dbsoft/startup/DBSAppStartup.java
+++ src/main/java/br/com/dbsoft/startup/DBSAppStartup.java
@@ -18,35 +18,20 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.apache.log4j.Logger;
+
+import br.com.dbsoft.startup.DBSApp.PROJECT_STAGE;
 import br.com.dbsoft.util.DBSNumber;
 
 /**
- * Classe para ser utilizada em substituição ao ServletContextListener.
- * Deve-se utilizar a anotação @WebListener em substuição da definição do listiner no web.xml
+ * Classe para ser utilizada em substituição ao ServletContextListener como listener inicial da aplicação.<br/>
+ * Os valores da classe DBSApp serão preenchidos automaticamente partir deste listener.
+ * Deve-se utilizar a anotação @WebListener em substuição da definição do listener no web.xml
  */
-public abstract class DBSStartup implements ServletContextListener{
+public abstract class DBSAppStartup implements ServletContextListener{
 
 	protected Logger wLogger =  Logger.getLogger(this.getClass());
 
 	private Integer wDelay = 4;
-	
-	private static String wAppName = null;
-	private static String wContextPath = null;
-	
-	private static URL wURLHttp = null;
-	private static URL wURLHttps = null;
-	
-	/**
-	 * URL do servidor HTTP
-	 * @return
-	 */
-	public static URL getURLHttp(){return wURLHttp;}
-	/**
-	 * URL do servidor HTTPs
-	 * @return
-	 */
-	public static URL getURLHttps(){return wURLHttps;}
-
 	
 	/**
 	 * Tempo antes de efetivamente inicializar o servidor.<br/>
@@ -67,12 +52,63 @@ public abstract class DBSStartup implements ServletContextListener{
 		}
 	}
 
-	private Runnable wRun = new Runnable(){
+	@Override
+	public void contextInitialized(ServletContextEvent pSce) {
+		if (pSce.getServletContext().getInitParameter("javax.faces.PROJECT_STAGE").toLowerCase().equals("production")){
+			DBSApp.ProjectStage = PROJECT_STAGE.PRODUCTION;
+		}else{
+			DBSApp.ProjectStage = PROJECT_STAGE.DEVELOPMENT;
+		}
+		DBSApp.AppName = pSce.getServletContext().getServletContextName();
+		DBSApp.ContextPath = pSce.getServletContext().getContextPath();
+		wLogger.info("STARTING:" + DBSApp.AppName + "\t" + DBSApp.ProjectStage.toString());
+		if (beforeStart()){
+			ScheduledExecutorService wScheduler = Executors.newSingleThreadScheduledExecutor();
+			wScheduler.schedule(wRun, getDelay(), TimeUnit.SECONDS);
+		}else{
+			onError();
+		}
+	}
 
+	@Override
+	public void contextDestroyed(ServletContextEvent pSce) {
+		wLogger.info("STOPPING:" + DBSApp.AppName);
+		afterStop();
+		wLogger.info("STOPPED:" + DBSApp.AppName);
+	}
+
+
+	/**
+	 * Método chamado antes do deploy efetivo. Não havendo serviços ativos ainda.<br/>
+	 * Caso queira efetuar chamadas http para a própria aplicação, por exemplo, utilize o <b>afterStart</b>.
+	 * @return
+	 */
+	public abstract boolean beforeStart();
+	
+	/**
+	 * Método chamado após o deploy.<br/>
+	 * A chamada a este método é efetuada com um delay mínimo, conforme definido no atributo.<br/> 
+	 * Este delay é necesário para aguardar o deploy total da aplicação.
+	 *
+	 */
+	public abstract void afterStart();
+	
+	
+	/**
+	 * Disparado quando no undeploy da aplicação.<br/>
+	 * Não é disparado caso o servidor de aplicação se ja interrompido. 
+	 */
+	public abstract void afterStop();
+
+	public void onError(){}
+
+	
+	//PRIVATE --------------------------------------------------------------------------------------------
+	private Runnable wRun = new Runnable(){
+	
 		@Override
 		public void run() {
 			boolean xOk = false;
-			wLogger.info("Starting:\t" + wAppName);
 			Long xTime = System.currentTimeMillis();
 			//Efetua nova tentativa até não ocorrer erro ou ultrapassar timeout de 10 segundos
 			while (!xOk){
@@ -81,9 +117,9 @@ public abstract class DBSStartup implements ServletContextListener{
 				}
 				xOk = getInfo();
 			}
-			if (xOk 
-			 && onStarted()){
-				wLogger.info("Started:\t" + wAppName);
+			if (xOk){ 
+				afterStart();
+				wLogger.info("STARTED:" + DBSApp.AppName);
 			}else{
 				onError();
 			}
@@ -99,10 +135,10 @@ public abstract class DBSStartup implements ServletContextListener{
 				xLocalHost = ManagementFactory.getPlatformMBeanServer().getAttribute(new ObjectName("jboss.as:interface=public"), "inet-address");
 				if (xLocalHost !=null){
 					if (xHttpPort != null){
-						wURLHttp = new URL("http",xLocalHost.toString(), DBSNumber.toInteger(xHttpPort.toString()), wContextPath);
+						DBSApp.URLHttp = new URL("http",xLocalHost.toString(), DBSNumber.toInteger(xHttpPort.toString()), DBSApp.ContextPath);
 					}
 					if (xHttpsPort != null){
-						wURLHttps = new URL("https",xLocalHost.toString(), DBSNumber.toInteger(xHttpsPort.toString()), wContextPath);
+						DBSApp.URLHttps = new URL("https",xLocalHost.toString(), DBSNumber.toInteger(xHttpsPort.toString()), DBSApp.ContextPath);
 					}
 				}
 				return true;
@@ -116,27 +152,5 @@ public abstract class DBSStartup implements ServletContextListener{
 			}
 			return false;
 		}
-	};
-	
-	@Override
-	public void contextInitialized(ServletContextEvent pSce) {
-		wContextPath = pSce.getServletContext().getContextPath();
-		wAppName = pSce.getServletContext().getContextPath();
-		ScheduledExecutorService wScheduler = Executors.newSingleThreadScheduledExecutor();
-		wScheduler.schedule(wRun, getDelay(), TimeUnit.SECONDS);
-	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent pSce) {
-		wLogger.info("Stopping:\t" + wAppName);
-		onStopped();
-		wLogger.info("Stopped:\t" + wAppName);
-	}
-
-
-	public abstract boolean onStarted();
-	
-	public abstract void onStopped();
-
-	public abstract void onError();
+	};;
 }
