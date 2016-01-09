6592f14cb627a104c35773db20cb52374520d8af
ricardo.villar <ricardo.villar@ricardo-villar>
2016-01-09 14:16:48 -0200

    Inclusão order por String e BigDecimal

--- src/main/java/br/com/dbsoft/comparator/DBSComparator.java
+++ src/main/java/br/com/dbsoft/comparator/DBSComparator.java
@@ -2,6 +2,7 @@ package br.com.dbsoft.comparator;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.Comparator;
@@ -9,6 +10,7 @@ import java.util.Comparator;
 import org.apache.log4j.Logger;
 
 import br.com.dbsoft.util.DBSIO;
+import br.com.dbsoft.util.DBSIO.SORT_DIRECTION;
 
 /**
  * Class para ordenar uma lista a partir dos atributos e respectivos valores.<br/>
@@ -24,20 +26,16 @@ public class DBSComparator<DataModelClass> implements Comparator<DataModelClass>
 	
 	protected Logger	wLogger = Logger.getLogger(this.getClass());
 	
-	public enum ORDER{
-		ASCENDING,
-		DESCENDING;
-	}
-	private String  wFieldName;
-	private ORDER	wOrder;
+	private String  		wFieldName;
+	private SORT_DIRECTION	wDirection;
 	
 	/**
 	 * @param pFieldName Nome do campos que será ordenado
-	 * @param pOrder Direção da ordenação
+	 * @param pDirection Direção da ordenação
 	 */
-	public DBSComparator(String pFieldName, ORDER pOrder) {
+	public DBSComparator(String pFieldName, SORT_DIRECTION pDirection) {
 		wFieldName = pFieldName;
-		wOrder = pOrder;
+		wDirection = pDirection;
 	}
 	
 	@Override
@@ -47,6 +45,11 @@ public class DBSComparator<DataModelClass> implements Comparator<DataModelClass>
 			Method xMethod1 = DBSIO.findDataModelMethod(pDataModel1.getClass(), "get" + wFieldName);
 			Method xMethod2 = DBSIO.findDataModelMethod(pDataModel2.getClass(), "get" + wFieldName);
 			//Executa o método para retornar o valor
+			if (xMethod1 == null){
+				wLogger.error("Atributo [" + wFieldName + "] não existente em " + pDataModel1.getClass().getName());
+				return 0;
+			}
+			
 			Object xValue1 = xMethod1.invoke(pDataModel1);
 			Object xValue2 = xMethod2.invoke(pDataModel2);
 			if (xValue1 == null &&
@@ -54,50 +57,56 @@ public class DBSComparator<DataModelClass> implements Comparator<DataModelClass>
 				return 0;
 			}
 			if (xValue1 == null){
-				if (wOrder == ORDER.ASCENDING){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
 					return -1;
 				}else{
 					return 1;
 				}
 			}else if (xValue2 == null){
-				if (wOrder == ORDER.ASCENDING){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
 					return 1;
 				}else{
 					return -1;
 				}
 			}
 			if (xValue1 instanceof Date){
-				if (wOrder == ORDER.ASCENDING){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
 					return ((Date) xValue1).compareTo((Date) xValue2);
 				}else{
 					return ((Date) xValue2).compareTo((Date) xValue1);
 				}
+			}else if (xValue1 instanceof String){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
+					return ((String) xValue1).compareTo((String) xValue2);
+				}else{
+					return ((String) xValue2).compareTo((String) xValue1);
+				}
 			}else if (xValue1 instanceof Double){
-				if (wOrder == ORDER.ASCENDING){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
 					return ((Double) xValue1).compareTo((Double) xValue2);
 				}else{
 					return ((Double) xValue2).compareTo((Double) xValue1);
 				}
 			}else if (xValue1 instanceof Integer){
-				if (wOrder == ORDER.ASCENDING){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
 					return ((Integer) xValue1).compareTo((Integer) xValue2);
 				}else{
 					return ((Integer) xValue2).compareTo((Integer) xValue1);
 				}
 			}else if (xValue1 instanceof Long){
-				if (wOrder == ORDER.ASCENDING){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
 					return ((Long) xValue1).compareTo((Long) xValue2);
 				}else{
 					return ((Long) xValue2).compareTo((Long) xValue1);
 				}
-			}else if (xValue1 instanceof Double){
-				if (wOrder == ORDER.ASCENDING){
-					return ((String) xValue1).compareTo((String) xValue2);
+			}else if (xValue1 instanceof BigDecimal){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
+					return ((BigDecimal) xValue1).compareTo((BigDecimal) xValue2);
 				}else{
-					return ((String) xValue2).compareTo((String) xValue1);
+					return ((BigDecimal) xValue2).compareTo((BigDecimal) xValue1);
 				}
 			}else if (xValue1 instanceof Timestamp){
-				if (wOrder == ORDER.ASCENDING){
+				if (wDirection == SORT_DIRECTION.ASCENDING){
 					return ((Timestamp) xValue1).compareTo((Timestamp) xValue2);
 				}else{
 					return ((Timestamp) xValue2).compareTo((Timestamp) xValue1);
