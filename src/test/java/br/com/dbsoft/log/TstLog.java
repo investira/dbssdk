56af70f5eb0f7b56e2da8da773629c833004270f
jose.avila <jose.avila@192.168.0.203>
2016-03-09 14:26:55 -0300

    Criado DBSDailyRollingFileAppender: Appender para Log que implementa uma propriedade de máximo de arquivos de backup, além de criar um arquivo novo a cada dia.

--- src/test/java/br/com/dbsoft/log/TstLog.java
+++ src/test/java/br/com/dbsoft/log/TstLog.java
@@ -3,21 +3,34 @@
  */
 package br.com.dbsoft.log;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+import java.net.MalformedURLException;
+
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 /**
  * @author Avila
- * 
  */
 public class TstLog {
 
-	@Test
+//	@Test
 	public void test_listarLog() {
 
 		assertNotNull("TESTE ESPERAVA NOTNULL",
 				DBSLog.loadLogFile("C:/jboss-as-7.1.1.Final/standalone/deployments/logs/ifeedin.log"));
 	}
+	
+	private static org.apache.log4j.Logger wLogger = Logger.getLogger(TstLog.class);
 
+	@Test
+	public void test_DBSDailyRollingFileAppender() throws MalformedURLException {
+		wLogger.trace("Trace");
+		wLogger.debug("Debug");
+		wLogger.info("Info");
+		wLogger.warn("Warn");
+		wLogger.error("Error");
+		wLogger.fatal("Fatal");
+	}
 }
