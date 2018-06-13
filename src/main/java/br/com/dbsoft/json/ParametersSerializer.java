6bba24e42e29210017530b2d416e69fdba2162cf
José Alberto Ávila <jose.avila@dbsoft.com.br>
2018-06-13 13:24:40 -0300

    Criada classe para estratégia de nomes JSON customizada;

--- src/main/java/br/com/dbsoft/json/ParametersSerializer.java
+++ src/main/java/br/com/dbsoft/json/ParametersSerializer.java
@@ -1,9 +1,15 @@
 package br.com.dbsoft.json;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
+
+import br.com.dbsoft.util.DBSString;
 
 public class ParametersSerializer {
 
@@ -21,4 +27,36 @@ public class ParametersSerializer {
 	}
 	
 	private ParametersSerializer() {}
+
+	public static class DBSPropertyNamingStrategy extends PropertyNamingStrategy {
+
+		private static final long serialVersionUID = 2803688738889527441L;
+
+	    @Override
+	    public String nameForGetterMethod(MapperConfig<?> pConfig, AnnotatedMethod pMethod, String pDefaultName) {
+	        return convert(pMethod, pMethod.getName().toString());
+	    }
+
+	    @Override
+	    public String nameForSetterMethod(MapperConfig<?> pConfig, AnnotatedMethod pMethod, String pDefaultName) {
+	        return convert(pMethod, pMethod.getName().toString());
+	    }
+
+	    private String convert(AnnotatedMethod pMethod, String pDefaultName) {
+	        Class<?> xClazz = pMethod.getDeclaringClass();
+	        Field[] xFields = xClazz.getDeclaredFields();
+	        for (Field xFld : xFields) {
+	        	String xMethodName = pDefaultName;
+	        	if (xMethodName.startsWith("get") || xMethodName.startsWith("set")) {
+	        		xMethodName = DBSString.getSubString(xMethodName, 4, xMethodName.length());
+	        	} else if (xMethodName.startsWith("is")) {
+	        		xMethodName = DBSString.getSubString(xMethodName, 3, xMethodName.length());
+	        	}
+	            if (xFld.getName().equalsIgnoreCase(xMethodName)) {
+	                return xFld.getName();
+	            }
+	        }
+	        return pDefaultName;
+	    }
+	}
 }
