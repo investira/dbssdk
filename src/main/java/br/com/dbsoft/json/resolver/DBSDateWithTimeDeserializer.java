464bbf2c609e8703e1fef25e9517065b28b78dc7
José Alberto Ávila <jose.avila@dbsoft.com.br>
2019-02-01 16:12:11 -0200

    Criado Serializer e Deserializer para sql.Date com Hora.

---
+++ src/main/java/br/com/dbsoft/json/resolver/DBSDateWithTimeDeserializer.java
@@ -0,0 +1,32 @@
+package br.com.dbsoft.json.resolver;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import br.com.dbsoft.util.DBSDate;
+
+public class DBSDateWithTimeDeserializer extends StdDeserializer<Date> {
+
+	private static final long serialVersionUID = -5787166120363721661L;
+	
+	private static String FORMAT = "dd/MM/yyyy hh:mm:ss";
+
+	public DBSDateWithTimeDeserializer() {
+		this(null);
+	}
+
+	public DBSDateWithTimeDeserializer(Class<?> pData) {
+		super(pData);
+	}
+
+	@Override
+	public Date deserialize(JsonParser pJSONParser, DeserializationContext pContext) throws IOException {
+		Date xDate = DBSDate.toDateCustom(pJSONParser.getText(), FORMAT);
+		return DBSDate.toDate(xDate.getTime());
+	}
+
+}
---
+++ src/main/java/br/com/dbsoft/json/resolver/DBSDateWithTimeSerializer.java
@@ -0,0 +1,32 @@
+package br.com.dbsoft.json.resolver;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import br.com.dbsoft.util.DBSFormat;
+
+public class DBSDateWithTimeSerializer extends StdSerializer<Date> {
+
+	private static final long serialVersionUID = -6244613685169545926L;
+
+	private static String FORMAT = "dd/MM/yyyy hh:mm:ss";
+
+	public DBSDateWithTimeSerializer() {
+		this(null);
+	}
+
+	public DBSDateWithTimeSerializer(Class<Date> pDateTime) {
+		super(pDateTime);
+	}
+
+	@Override
+	public void serialize(Date pValue, JsonGenerator pGen, SerializerProvider pArg2) throws IOException, JsonProcessingException {
+		pGen.writeString(DBSFormat.getFormattedDateCustom(pValue, FORMAT));
+	}
+
+}
